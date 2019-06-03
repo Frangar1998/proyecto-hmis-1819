89708ab519b107c9025bae7dfb6978f452ac497d
unknown <fgl244@inlumine.ual.es>
2019-06-03 16:24:38 +0200

    Creados nuevos login y register

---
+++ frontend/src/app/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [RegisterComponent, LoginComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
