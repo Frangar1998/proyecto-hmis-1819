0a6ca290254a8f35f68937d2d034f20f92c35d9f
Alejandro Francisco García Uclés <agu528@inlumine.ual.es>
2019-06-04 17:44:55 +0200

    Añadido error registro y proceso de error login

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -3,10 +3,11 @@ import { HttpClient } from '@angular/common/http';
 //import { HttpClientModule } from '@angular/common/http';
 import { User } from  './user';
 import { JwtResponse } from  './jwt-response';
-import { tap } from  'rxjs/operators';
-import { Observable, BehaviorSubject } from  'rxjs';
+import { tap, map, catchError } from  'rxjs/operators';
+import { Observable, BehaviorSubject, throwError } from  'rxjs';
 import { Validators } from '@angular/forms';
 import { registerContentQuery } from '@angular/core/src/render3';
+import { ObserveOnOperator } from 'rxjs/internal/operators/observeOn';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,19 @@ export class AuthService {
   authSubject  =  new  BehaviorSubject(false);
   errorEmail:boolean = false;
   errorLogin:boolean= false;
+
+  /*
+  private subject = new BehaviorSubject<string>("");
+  private pullerObserver:Observable<string> = this.subject;
+*/
+
   constructor(private httpClient: HttpClient) { }
+
+  /*
+  subscribe(observer){
+    return this.pullerObserver.subscribe(observer);
+  }
+  */
 
   register(user: User): Observable<JwtResponse> {
       return this.httpClient.post<JwtResponse>(`${this.AUTH_SERVER}/register`, user).pipe(
@@ -39,10 +52,8 @@ export class AuthService {
 
 
   signIn(user: User): Observable<JwtResponse> {
-    
     return this.httpClient.post(`${this.AUTH_SERVER}/login`, user).pipe(
       tap(async (res: JwtResponse) => {
-
         if (res.user) {
           localStorage.setItem("ACCESS_TOKEN", res.user.access_token);
           this.authSubject.next(true);
@@ -50,21 +61,30 @@ export class AuthService {
       })
     );
   }
-
-  comprobarLogin(user:User){
-    this.httpClient.post(`${this.AUTH_SERVER}/login`, user).pipe(
-      tap(async (res: JwtResponse) => {
-        console.log("loquesea");
-        if (res.user) {
-          this.errorLogin = false;
-        }else{
-          this.errorLogin = true;
+  /*
+    comprobarLogin(user:User){
+      return new Observable<User>(observer =>{
+        this.httpClient.post(`${this.AUTH_SERVER}/login`, user).pipe(
+          
+          map(e =>e as User),
+          catchError(e =>throwError(e))
+        ).subscribe(user => {
+          console.log("llego1");
+          observer.next(user)
+          observer.complete();
+        },err => {
+          console.log("llego2");
+          observer.error(new Error(err.message ? err.message : err))
+          observer.complete();
+          //this.errorLogin=true;
         }
-      })
-    );
-    //if(this.httpClient.post(`${this.AUTH_SERVER}/login`, user).){}
-  }
 
+        )
+      });
+      
+      //if(this.httpClient.post(`${this.AUTH_SERVER}/login`, user).){}
+    }
+  */
   signOut() {
     localStorage.removeItem("ACCESS_TOKEN");
     localStorage.removeItem("EXPIRES_IN");
--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -12,22 +12,17 @@ import { User } from '../user';
 export class LoginComponent implements OnInit {
 
   constructor(private authService: AuthService, private router: Router) { }
-  errorLogin:boolean= false;
+
   ngOnInit() {
   }
 
   login(form){
     console.log(form.value);
-    this.authService.comprobarLogin(form.value); 
-    if(this.authService.errorLogin){
-      this.errorLogin = true;
-    }else{
-      this.authService.signIn(form.value).subscribe((res)=>{
-        console.log("Logged in!");
-        this.router.navigateByUrl('/auth/home');
-      });  
-    }
-      
+    this.authService.signIn(form.value).subscribe((res)=>{
+      console.log("Logged in!");
+      this.router.navigateByUrl('/auth/home');
+    });    
+    
   }
 
   register(){
